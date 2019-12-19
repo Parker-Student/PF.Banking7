664864093afc4d8afd9ad760139db31b93729aed
Parker Foord <700214020@fvtc.edu>
2019-12-18 18:43:31 -0600

    Add project files.

---
+++ Banking7/frmBanking7.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banking7
+{
+    public partial class frmBanking7 : Form
+    {
+        public frmBanking7()
+        {
+            InitializeComponent();
+        }
+    }
+}
